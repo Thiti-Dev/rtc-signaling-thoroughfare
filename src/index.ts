1c3178131b2f04ad7aa3b0c017dbc3f38ff3527a
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-02-08 17:06:46 +0700

    finalizing entrance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world")
+import fastify from "fastify";
+import { Server } from "socket.io";
+import { boilatePlugins } from "./plugins/boilate.js";
+import { socketEventInitializer } from "./core/index.js";
+const server = fastify();
+boilatePlugins(server) // setting up the fastify plugin . . .
+
+server.ready().then(() => {
+  socketEventInitializer(server)
+});
+
+server.listen({ port: 3000 });
+declare module "fastify" {
+    interface FastifyInstance {
+      io: Server;
+    }
+  }
