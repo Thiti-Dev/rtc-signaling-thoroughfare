0f514e5bc409ec3eeaa7fc1d326db16d9d9dd906
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-02-12 01:11:52 +0700

    chore: port derived or fixed from env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import fastify from "fastify";
 import { Server } from "socket.io";
 import { boilatePlugins } from "./plugins/boilate.js";
 import { socketEventInitializer } from "./core/index.js";
+
+const PORT: number = parseInt(process.env.PORT!) || 300
+
 const server = fastify();
 boilatePlugins(server) // setting up the fastify plugin . . .
 
@@ -9,7 +12,7 @@ server.ready().then(() => {
   socketEventInitializer(server)
 });
 
-server.listen({ port: 3000 });
+server.listen({ port: PORT,host:'0.0.0.0' });
 declare module "fastify" {
     interface FastifyInstance {
       io: Server;
